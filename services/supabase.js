0751963c637b6f46cf9c9173c3e0cd740f6a7465
caiofernandosa-dev <caiofernandosa@gmail.com>
2026-05-26 17:19:50 -0400

    Update supabase.js

--- services/supabase.js
+++ services/supabase.js
@@ -2,7 +2,13 @@ const { createClient } = require('@supabase/supabase-js');
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
+  process.env.SUPABASE_KEY,
+  {
+    auth: { persistSession: false },
+    global: {
+      fetch: (...args) => import('node-fetch').then(({default: f}) => f(...args))
+    }
+  }
 );
 
 module.exports = supabase;
